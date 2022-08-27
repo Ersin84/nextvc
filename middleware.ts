984a23c1bcf1b46db2339b8a086a36f446dedfc8
wromo <info.wromo@gmail.com>
2022-08-27 08:57:53 +0200

    add-2

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 // The country to block from accessing the secret page
-const BLOCKED_COUNTRY = 'AT';
+const BLOCKED_COUNTRY = 'US';
 
 // Trigger this middleware to run on the `/secret-page` route
 export const config = {
@@ -13,7 +13,7 @@ export const config = {
 
 export function middleware(req: NextRequest) {
   // Extract country. Default to US if not found.
-  const country = (req.geo && req.geo.country) || 'AT';
+  const country = (req.geo && req.geo.country) || 'US';
 
   console.log(`Visitor from ${country}`);
 
